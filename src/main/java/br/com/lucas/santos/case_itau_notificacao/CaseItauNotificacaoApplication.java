47759a05d47511e1f208222aea952c817ff21e01
Lucas <lucasvinipro23@gmail.com>
2025-04-18 18:32:17 -0300

    criação do projeto

---
+++ src/main/java/br/com/lucas/santos/case_itau_notificacao/CaseItauNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.santos.case_itau_notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseItauNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseItauNotificacaoApplication.class, args);
+	}
+
+}
